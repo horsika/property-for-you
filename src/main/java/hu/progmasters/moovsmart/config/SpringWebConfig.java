0016e3cd36205b3af850b0316290322e54ce0ec0
pintereva <pinterevalive@gmail.com>
2023-08-28 15:48:35 +0200

    SecurityConfiguration update

--- src/main/java/hu/progmasters/moovsmart/config/SpringWebConfig.java
+++ src/main/java/hu/progmasters/moovsmart/config/SpringWebConfig.java
@@ -18,10 +18,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringWebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+//                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
+//    }
 }
