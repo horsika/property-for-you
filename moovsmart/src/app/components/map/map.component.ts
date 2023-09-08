cecb875a93cb1444243c39006661407f9de9fbbb
pintereva <pinterevalive@gmail.com>
2023-09-08 08:47:28 +0200

    custom icon and popup upgrade

--- moovsmart/src/app/components/map/map.component.ts
+++ moovsmart/src/app/components/map/map.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import {PropertyService} from "../../services/property.service";
 import {PropertyListItemModel} from "../../models/propertyListItem.model";
-
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-map',
@@ -14,7 +14,8 @@ export class MapComponent implements AfterViewInit {
   private map: any;
   private isMapInitialized = false;
 
-  constructor(private propertyService: PropertyService) {
+  constructor(private propertyService: PropertyService,
+              private router: Router) {
   }
 
   ngAfterViewInit() {
@@ -53,31 +54,52 @@ export class MapComponent implements AfterViewInit {
 
   displayPropertiesOnMap(properties: Array<PropertyListItemModel>): void {
 
-    if (!this.map){
+    if (!this.map) {
       return;
     }
 
     const propertyMarkers: L.Marker[] = [];
 
-    this.map.eachLayer((layer: any) =>{
+    this.map.eachLayer((layer: any) => {
       if (layer instanceof L.Marker) {
         this.map.removeLayer(layer)
       }
     })
+
     for (const property of properties) {
       const lat = parseFloat(String(property.latitude));
       const lon = parseFloat(String(property.longitude));
-      const propertyMarker = L.marker([lat, lon], {
-        icon: L.icon({
-          iconUrl: 'assets/leaflet/marker-icon.png',
-          shadowUrl: 'assets/leaflet/marker-shadow.png',
-          iconSize: [25, 41], // size of the icon
-          iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
-          shadowSize: [41, 41], // size of the shadow
-          shadowAnchor: [12, 41], // the same for the shadow
-        })
+
+      // Create a custom popup content with an image
+      const popupContent = `
+    <div>
+      <img src="${property.images[0]}" alt="${property.name}" style="max-width: 80%; max-height: 45%;">
+      <p>${this.formatPrice(property.price)}</p>
+      <p>${property.floorArea} sqft</p>
+      <p>${property.name} sqft</p>
+    </div>
+  `;
+
+      console.log('Property Price:', property.price);
+
+      // Create a custom icon
+      const customIcon = L.divIcon({
+        className: 'custom-icon',
+        html: '<i class="fas fa-building" style="color: #d922dd; font-size: 20px;"></i>',
+        iconAnchor: [12, 41], // Adjust anchor point if necessary
       });
-      propertyMarker.bindPopup(property.name);
+
+      const propertyMarker = L.marker([lat, lon], {
+        icon: customIcon,
+      });
+
+      propertyMarker.bindPopup(popupContent);
+
+      propertyMarker.on('popupclose', () => {
+        this.propertyService.goToPropertyDetails(property.propertyId);
+      });
+
+
       propertyMarker.addTo(this.map);
       propertyMarkers.push(propertyMarker);
     }
@@ -87,6 +109,14 @@ export class MapComponent implements AfterViewInit {
       const markerBounds = L.featureGroup(propertyMarkers).getBounds();
       this.map.fitBounds(markerBounds);
     }
+  }
+
+  formatPrice(price: number): string {
+    // Perform formatting logic here
+    return price.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'HUF',
+      maximumFractionDigits: 0});
   }
 
 
