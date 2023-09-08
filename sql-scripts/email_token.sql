5905aab3736a35b088b8b2c500954a5e8e5abd3d
pintereva <pinterevalive@gmail.com>
2023-09-08 13:05:57 +0200

    csv and sql

---
+++ sql-scripts/email_token.sql
@@ -0,0 +1,5 @@
+INSERT INTO moovsmart.email_token (id, expiry_date_time, token, user_id) VALUES (1, '2023-09-08 08:32:31.967607', 'af7bb966-fb66-4f0c-b069-a3fba2e68f7d', 7);
+INSERT INTO moovsmart.email_token (id, expiry_date_time, token, user_id) VALUES (2, '2023-09-08 08:32:33.421002', '101bf16c-5602-4b19-86ee-8f2e48949c98', 8);
+INSERT INTO moovsmart.email_token (id, expiry_date_time, token, user_id) VALUES (3, '2023-09-08 10:51:19.581796', '45dcfb4e-afb1-48f1-98f5-7c4358c50933', 9);
+INSERT INTO moovsmart.email_token (id, expiry_date_time, token, user_id) VALUES (4, '2023-09-09 08:31:11.335174', 'adea242f-8c12-478c-b5a0-045790ea61da', 10);
+INSERT INTO moovsmart.email_token (id, expiry_date_time, token, user_id) VALUES (5, '2023-09-09 08:33:20.474878', '4b7c4e66-d9e1-4711-9162-18a434f33705', 11);
