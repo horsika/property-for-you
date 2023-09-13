ad49d1358d1369d7811532b5aa85877d4bd70f05
Orsi <horsika97@gmail.com>
2023-09-13 17:22:23 +0200

    file validation on propertyform

--- moovsmart/src/app/components/property-form/property-form.component.ts
+++ moovsmart/src/app/components/property-form/property-form.component.ts
@@ -2,53 +2,55 @@ import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {PropertyService} from '../../services/property.service';
 import {Router} from '@angular/router';
-import {validationHandler} from '../../utils/validationHandler';
+import {errorHandler, validationHandler} from '../../utils/validationHandler';
 import {PropertyTypeFormListItemModel} from "../../models/property-type-form-list-item.model";
 import {HeatingTypeFormListItemModel} from "../../models/heating-type-form-list-item.model";
 import {validateNumberOfBathrooms} from "../../utils/custom.validators";
 
 @Component({
-    selector: 'app-property-form',
-    templateUrl: './property-form.component.html',
-    styleUrls: ['./property-form.component.css'],
+  selector: 'app-property-form',
+  templateUrl: './property-form.component.html',
+  styleUrls: ['./property-form.component.css'],
 })
 export class PropertyFormComponent implements OnInit {
 
-    propertyForm: FormGroup;
-    propertyTypeList: PropertyTypeFormListItemModel[];
-    heatingTypeList: HeatingTypeFormListItemModel[];
+  propertyForm: FormGroup;
+  propertyTypeList: PropertyTypeFormListItemModel[];
+  heatingTypeList: HeatingTypeFormListItemModel[];
+  errorMessage: string | null = null;
 
-    constructor(private formBuilder: FormBuilder,
-                private propertyService: PropertyService,
-                private router: Router) {
-        this.propertyForm = this.formBuilder.group({
-            'name': ['', [Validators.required, Validators.minLength(3)]],
-            'numberOfBedrooms': ['', [Validators.required, Validators.min(1)]],
-            'numberOfBathrooms': ['', [Validators.required, Validators.min(1), validateNumberOfBathrooms]],
-            'price': ['', [Validators.required, Validators.min(0.1)]],
-            'floorArea': ['', [Validators.required, Validators.min(5)]],
-            'airConditioning': [false],
-            'description': ['', [Validators.required, Validators.maxLength(600), Validators.minLength(50)]],
-            'images': this.formBuilder.array([]),
-            'address': ['', [Validators.required]],
-            'propertyType': ['', Validators.required],
-            'heatingType': ['', Validators.required],
-            'listingStatus': [''],
-            'listingType': ['', Validators.required],
-        });
-    }
+  constructor(private formBuilder: FormBuilder,
+              private propertyService: PropertyService,
+              private router: Router) {
+    this.propertyForm = this.formBuilder.group({
+      'name': ['', [Validators.required, Validators.minLength(3)]],
+      'numberOfBedrooms': ['', [Validators.required, Validators.min(1)]],
+      'numberOfBathrooms': ['', [Validators.required, Validators.min(1), validateNumberOfBathrooms]],
+      'price': ['', [Validators.required, Validators.min(0.1)]],
+      'floorArea': ['', [Validators.required, Validators.min(5)]],
+      'airConditioning': [false],
+      'description': ['', [Validators.required, Validators.maxLength(600), Validators.minLength(50)]],
+      'images': this.formBuilder.array([]),
+      'address': ['', [Validators.required]],
+      'propertyType': ['', Validators.required],
+      'heatingType': ['', Validators.required],
+      'listingStatus': [''],
+      'listingType': ['', Validators.required],
+    });
+  }
 
-    ngOnInit() {
-        this.propertyService.getFormOptions().subscribe({
-            next: response => {
-                this.propertyTypeList = response.propertyTypes;
-                this.heatingTypeList = response.heatingTypes;
-            },
-            error: err => console.warn(err),
-        })
-    }
+  ngOnInit() {
+    this.propertyService.getFormOptions().subscribe({
+      next: response => {
+        this.propertyTypeList = response.propertyTypes;
+        this.heatingTypeList = response.heatingTypes;
+      },
+      error: err => console.warn(err),
+    })
+  }
 
   onFileChange(event: any) {
+    this.errorMessage = null;
     if (event?.target?.files?.length > 0) {
       const images = event.target.files;
       const imageControls = this.propertyForm.get('images') as FormArray;
@@ -60,28 +62,31 @@ export class PropertyFormComponent implements OnInit {
     }
   }
 
-    submit = () => {
-      const formData = new FormData();
-      // Loop through the controls in your FormGroup
-      Object.keys(this.propertyForm.controls).forEach((key) => {
-        const control = this.propertyForm.get(key);
-        if (control instanceof FormControl) {
-          formData.append(key, control.value);
-        } else if(control instanceof FormArray){
-          for (const fileControl of control.controls) {
-            formData.append('images', fileControl.value);
-          }
+  submit = () => {
+    const formData = new FormData();
+    // Loop through the controls in your FormGroup
+    Object.keys(this.propertyForm.controls).forEach((key) => {
+      const control = this.propertyForm.get(key);
+      if (control instanceof FormControl) {
+        formData.append(key, control.value);
+      } else if (control instanceof FormArray) {
+        for (const fileControl of control.controls) {
+          formData.append('images', fileControl.value);
         }
-      });
+      }
+    });
 
-        this.propertyService.createProperty(formData).subscribe(
-            () => this.router.navigate(['/property-list'], {queryParams: {city: this.getCityFromAddress()}}),
-            error => validationHandler(error, this.propertyForm),
-        );
+    this.propertyService.createProperty(formData).subscribe(
+      () => this.router.navigate(['/property-list'], {queryParams: {city: this.getCityFromAddress()}}),
+      error => {
+        validationHandler(error, this.propertyForm)
+        this.errorMessage = errorHandler(error)
+      },
+    );
 
-    };
+  };
 
-    getCityFromAddress() {
-        return this.propertyForm.get('address').value.split(' ')[1];
-    }
+  getCityFromAddress() {
+    return this.propertyForm.get('address').value.split(' ')[1];
+  }
 }
