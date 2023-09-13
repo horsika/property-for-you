341a885b73a53057f125b57545cc24eefa45c7b9
pintereva <pinterevalive@gmail.com>
2023-09-13 08:31:33 +0200

    openhouse form (html, ts)

--- moovsmart/src/app/components/my-page/my-page.component.ts
+++ moovsmart/src/app/components/my-page/my-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {errorHandler, validationHandler} from "../../utils/validationHandler";
 import {MyAccountModel} from "../../models/my-account.model";
 import {PropertyService} from "../../services/property.service";
@@ -9,6 +9,8 @@ import {MyPropertyListItemModel} from "../../models/my-property-list-item.model"
 import {PropertyActiveToggleModel} from "../../models/property-active-toggle.model";
 import {PasswordChangeModel} from "../../models/password-change.model";
 import {AdminService} from "../../services/admin.service";
+import {OpenHouseService} from "../../services/open-house.service";
+import {OpenHouseFormDataModel} from "../../models/open-house-form-data.model";
 
 @Component({
   selector: 'app-my-page',
@@ -25,8 +27,10 @@ export class MyPageComponent implements OnInit {
   mySavedProperties: MyPropertyListItemModel[];
   password: FormGroup;
   profilePic: FormGroup;
+  openHouse: FormGroup;
+  selectedPropertyId: number  | null = null;
 
-  constructor(private userService: UserService, private propertyService: PropertyService, private router: Router, private formBuilder: FormBuilder, private adminService: AdminService) {
+  constructor(private userService: UserService, private propertyService: PropertyService, private router: Router, private formBuilder: FormBuilder, private adminService: AdminService, private openHouseService: OpenHouseService) {
     this.email = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]]
     })
@@ -38,6 +42,15 @@ export class MyPageComponent implements OnInit {
     this.profilePic = this.formBuilder.group({
       file: [null]
     })
+
+    this.openHouse = this.formBuilder.group({
+      propertyId: new FormControl(this.selectedPropertyId),
+      fromTime: ['', Validators.required],
+      toTime:['', Validators.required],
+      maxParticipants: [1, [Validators.required, Validators.min(1), Validators.max(20) ]],
+    })
+    this.openHouse.get('propertyId').valueChanges.subscribe((newPropertyId) => {
+    });
   }
 
   // -------------- DECIDING WHICH PAGE TO DISPLAY ------------------
@@ -78,6 +91,12 @@ export class MyPageComponent implements OnInit {
     this.propertyService.getMySavedProperties().subscribe(response => {
       this.mySavedProperties = response;
     })
+  }
+
+  showOpenHouseForm(propertyId: number) {
+    this.activePage = 'OpenHouse';
+    this.selectedPropertyId = propertyId;
+    this.openHouse.get('propertyId').setValue(this.selectedPropertyId);
   }
 
   // ------------------- FUNCTIONS -------------------------
@@ -167,5 +186,31 @@ export class MyPageComponent implements OnInit {
       () => {
       this.showAccountDetails();
       })
+  }
+
+
+  getSelectedPropertyName() {
+    const selectedProperty = this.myProperties.find(
+      (property) => property.propertyId === this.selectedPropertyId
+    );
+
+    if (selectedProperty && selectedProperty.name) {
+      return selectedProperty.name;
+    } else {
+      return '---Choose your property!---';
+    }
+  }
+
+  createOpenHouse(){
+    const data: OpenHouseFormDataModel = this.openHouse.value;
+    this.openHouseService.createOpenHouse(data).subscribe({
+      next: () => {
+        console.log('Open house is created.');
+      },
+      error: err => validationHandler(err, this.openHouse),
+      complete: () => {
+        this.showMyProperties()
+      }
+    })
   }
 }
